258a23bddcd43343523177453bfaa4b9c2d3aa27
nitanmarcel <nitan.marcel@gmail.com>
2019-02-12 21:08:44 +0200

    Change GetInstalledPlugins plugin code to match latest companion edits

--- getinstalledplugins/getinstalledplugins.py
+++ getinstalledplugins/getinstalledplugins.py
@@ -1,14 +1,14 @@
 from tg_companion.tgclient import client
 from telethon import events
-from tg_companion.plugins import PluginManager
-
+from tg_companion.pluginmanager import load_plugins_info
+from tg_companion.plugins import load_plugins
 
 @client.on(events.NewMessage(outgoing=True, pattern=r"\.plugin (.+)"))
-async def testplugin(e):
-    plugin_name = e.pattern_match.group(1)
+async def get_plugin_info(event):
+    plugin_name = event.pattern_match.group(1)
     OUTPUT = f"Plugin Info For: {plugin_name}\n\n"
 
-    plugins = PluginManager.load_plugins_info()
+    plugins = load_plugins_info()
     if plugin_name in plugins:
 
         dct = plugins[plugin_name]
@@ -16,15 +16,15 @@ async def testplugin(e):
         for k, v in dct.items():
             OUTPUT += f"\n{k} : `{v}`"
 
-        await e.reply(OUTPUT)
+        await event.reply(OUTPUT)
     else:
-        await e.edit(f"Plugin `{plugin_name}` is not installed")
+        await event.edit(f"Plugin `{plugin_name}` is not installed")
 
 
 @client.on(events.NewMessage(outgoing=True, pattern=r"\.plugins"))
-async def get_installed_plugins(e):
-    PLUGINS = sorted(PluginManager.load_plugins())
+async def get_installed_plugins(event):
+    PLUGINS = sorted(load_plugins())
     OUTPUT = f"Installed Plugins:\n\n"
     for plugin in PLUGINS:
         OUTPUT += f"`\n{plugin}`"
-    await e.reply(OUTPUT)
+    await event.reply(OUTPUT)
